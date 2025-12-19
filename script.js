d95ce72db53fc59d3ce230af849e1d297f2ba111
Azhan Zulfiqar <126256157+azhan-code@users.noreply.github.com>
2025-12-20 03:26:57 +0500

    Add basic calculator functions to script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function appendToDisplay(input) { document.getElementById("display").value += input; }
+function clearDisplay() { document.getElementById("display").value = ""; }
+function calculate() {
+    try { document.getElementById("display").value = eval(document.getElementById("display").value); }
+    catch(error) { document.getElementById("display").value = "Error"; }
+}
